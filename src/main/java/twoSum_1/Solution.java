95113a1ee2591b5618ecaaf7cc8c4738e545ab6f
张艺坤 <zhangykm@yonyou.com>
2019-10-08 14:35:03 +0800

    init

---
+++ src/main/java/twoSum_1/Solution.java
@@ -0,0 +1,28 @@
+package twoSum_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("没有符合目标值的两个数！");
+    }
+
+
+    public static void main(String args[]) {
+        Solution solution = new Solution();
+        int[] nums = {2,3,4,5,6,7,2,12,13,12};
+        int[] twoNum = solution.twoSum(nums, 99);
+
+    }
+
+}
