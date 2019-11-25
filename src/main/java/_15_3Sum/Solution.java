93247e619b63300e0f61427ad1337f8006c4a4c0
张艺坤 <zhangykm@yonyou.com>
2019-11-25 10:49:54 +0800

    update to 17

--- src/main/java/_15_3Sum/Solution.java
+++ src/main/java/_15_3Sum/Solution.java
@@ -47,7 +47,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] num = {1, 0, 1, 2, -1, -4};
+        int[] num = {-1, 0, 1, 2, -1, -4};
         Solution solution = new Solution();
         System.out.println(solution.threeSum(num));
     }
